dd7cb7d2bca495120f22cace2c069ab837464a94
Noahiemt <duckert1237@gmail.com>
2025-02-07 12:20:39 +0100

    Ryddet opp filstruktur

---
+++ Oblig1/HomeExam01-Handout/build/CMakeFiles/stringops.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/uio/hume/student-u73/niteigen/Desktop/IN2140/HomeExam01-Handout/stringops-main.c" "CMakeFiles/stringops.dir/stringops-main.c.o" "gcc" "CMakeFiles/stringops.dir/stringops-main.c.o.d"
+  "/uio/hume/student-u73/niteigen/Desktop/IN2140/HomeExam01-Handout/stringops-todo.c" "CMakeFiles/stringops.dir/stringops-todo.c.o" "gcc" "CMakeFiles/stringops.dir/stringops-todo.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
